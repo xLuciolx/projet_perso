30f584995a5153b81c602658f33a77bcccbbb61c
Loïc Gallay <lgallay@orange.fr>
2017-07-23 19:53:47 +0200

    Start HTML structure

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="CV développeur intégrateur web: html, css, javascript, php, sql.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CV Développeur / Intégrateur Web - Loïc Gallay </title>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Materialize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <div class="container">
+    <header>
+      <div class="center-align">
+        <p>
+          Loïc Gallay
+        </p>
+        <h1>
+          Développeur / Intégrateur Web
+        </h1>
+      </div>
+    </header>
+
+    <nav class="hide-on-small-only col m12 l12 blue-grey lihghten-1">
+        <ul>
+          <li>
+            <a href="#about" class="active">A propos</a>
+          </li>
+          <li>
+            <a href="#skill">Compétences</a>
+          </li>
+          <li>
+            <a href="#work">Réalisation</a>
+          </li>
+          <li>
+            <a href="#resume">CV express</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+    </nav>
+
+    <div class="fixed-action-btn horizontal click-to-toggle show-on-small hide-on-med-and-up" >
+      <a class="btn-floating btn-large blue-grey lighten-1"><i class="material-icons">menu</i></a>
+      <ul>
+        <li><a href="#about" class="btn-floating red"><i class="material-icons">person_outline</i></a></li>
+        <li><a href="#skill" class="btn-floating yellow darken-1"><i class="material-icons">school</i></a></li>
+        <li><a href="#work" class="btn-floating green"><i class="material-icons">build</i></a></li>
+        <li><a href="#resume" class="btn-floating green"><i class="material-icons">insert_drive_file</i></a></li>
+        <li><a href="#contact" class="btn-floating blue"><i class="material-icons">mail_outline</i></a></li>
+      </ul>
+    </div>
+    <div class="divider hide-om-med-and-up show-on-small"></div>
+    <main>
+
+      <section id="about">
+        <div class="row">
+          <div class="col s12 m12 l12 center-align">
+            <h2>
+              A propos
+            </h2>
+            <p>
+              Quelques mots pour me décrire
+            </p>
+          </div>
+          <div class="row">
+            <div class="col s12 m4 l4 push-m8 push-l8">
+              <img src="img/avatar.jpg" alt="avatar" class="responsive-img circle center-block">
+            </div>
+            <div class="col s12 m8 l8 pull-m4 pull-l4">
+              <p>
+                Développeur / Intégrateur web curieux, autonome et rigoureux, j'aime les applications simples, rapides et efficaces.
+                Un sens de l'écoute et du service, une expertise technique en constante progression grâce à une formation continue. Motivé par le besoin de toujours mieux faire et appuyé par des bases acquises au cours de mon parcours, je prend plaisir à relever de nouveaux challenges.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="divider"></div>
+      <section id="skill">
+        Compétence
+      </section>
+      <div class="divider"></div>
+      <section id="work">
+        Réalisation
+      </section>
+      <div class="divider"></div>
+      <section id="resume">
+        CV express
+      </section>
+      <div class="divider"></div>
+      <section id="contact">
+        Contact
+      </section>
+
+    </main>
+
+    <footer>
+    </footer>
+  </div>
+
+
+
+
+
+
+  <!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- Javascript Materialize -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
+  <script src="js/script.js" charset="utf-8"></script>
+</body>
+</html>
