d520ee129a4d90faf867c435ea81bf60d106995a
Loïc Gallay <lgallay@orange.fr>
2017-07-24 23:17:08 +0200

    Continue HTML structure, almost finish skill section

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  include 'include/tool.php';
+ ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -28,6 +32,7 @@
     </header>
 
     <nav class="hide-on-small-only col m12 l12 blue-grey lihghten-1">
+        <!-- <ul class="table-of-contents"> -->
         <ul>
           <li>
             <a href="#about" class="active">A propos</a>
@@ -36,7 +41,7 @@
             <a href="#skill">Compétences</a>
           </li>
           <li>
-            <a href="#work">Réalisation</a>
+            <a href="#work">Réalisations</a>
           </li>
           <li>
             <a href="#resume">CV express</a>
@@ -48,11 +53,11 @@
     </nav>
 
     <div class="fixed-action-btn horizontal click-to-toggle show-on-small hide-on-med-and-up" >
-      <a class="btn-floating btn-large blue-grey lighten-1"><i class="material-icons">menu</i></a>
+      <a class="btn-floating btn-large blue-grey lighten-1 waves-effect waves-light"><i class="material-icons">menu</i></a>
       <ul>
         <li><a href="#about" class="btn-floating red"><i class="material-icons">person_outline</i></a></li>
         <li><a href="#skill" class="btn-floating yellow darken-1"><i class="material-icons">school</i></a></li>
-        <li><a href="#work" class="btn-floating green"><i class="material-icons">build</i></a></li>
+        <li><a href="#work" class="btn-floating deep-orange"><i class="material-icons">build</i></a></li>
         <li><a href="#resume" class="btn-floating green"><i class="material-icons">insert_drive_file</i></a></li>
         <li><a href="#contact" class="btn-floating blue"><i class="material-icons">mail_outline</i></a></li>
       </ul>
@@ -60,7 +65,7 @@
     <div class="divider hide-om-med-and-up show-on-small"></div>
     <main>
 
-      <section id="about">
+      <section id="about" class="scrollspy">
         <div class="row">
           <div class="col s12 m12 l12 center-align">
             <h2>
@@ -83,20 +88,183 @@
           </div>
         </div>
       </section>
+
       <div class="divider"></div>
-      <section id="skill">
-        Compétence
+
+      <section id="skill" class="scrollspy">
+        <div class="row">
+          <div class="col s12 center-align">
+            <h2>
+              Compétences
+            </h2>
+            <p>
+              En formation continue.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m4 l4">
+            <div class="flip-container">
+	            <div class="flipper">
+		            <div class="front">
+			            <!-- front content -->
+                  <div class="card medium">
+                    <div class="card-content">
+                      <h3 class="center-align">
+                        Front-end
+                      </h3>
+                    </div>
+                  </div>
+		            </div>
+		            <div class="back">
+			            <!-- back content -->
+                  <div class="card medium z-depth-4">
+                    <div class="card-content">
+                      <span class="card-title">Front-end</span>
+                      <ul>
+                        <li>
+                          <p>
+                            HTML
+                          </p>
+                          <?php starIcon('star', 5); ?>
+                        </li>
+                        <li>
+                          <p>
+                            CSS
+                          </p>
+                          <?php
+                            starIcon('star', 4);
+                            starIcon('star_half', 1);
+                           ?>
+                        </li>
+                        <li>
+                          <p>
+                            Javascript
+                          </p>
+                          <?php
+                            starIcon('star', 4);
+                            starIcon('star_border', 1);
+                           ?>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+		            </div>
+	            </div>
+            </div>
+          </div>
+          <div class="col s12 m4 l4">
+            <div class="flip-container">
+              <div class="flipper">
+                <div class="front">
+                  <!-- front content -->
+                  <div class="card medium">
+                    <div class="card-content">
+                      <h3 class="center-align">
+                        Back-end
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="back">
+                  <!-- back content -->
+                  <div class="card medium z-depth-4">
+                    <div class="card-content">
+                      <span class="card-title">Back-end</span>
+                      <ul>
+                        <li>
+                          <p>
+                            PHP
+                          </p>
+                          <?php starIcon('star', 4); ?>
+                          <?php starIcon('star_border', 1); ?>
+                        </li>
+                        <li>
+                          <p>
+                            PHP orienté objet
+                          </p>
+                          <?php
+                            starIcon('star_border', 5);
+                           ?>
+                        </li>
+                        <li>
+                          <p>
+                            SQL
+                          </p>
+                          <?php
+                            starIcon('star', 4);
+                            starIcon('star_border', 1);
+                           ?>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col s10 offset-s1 m4 l4">
+            <div class="flip-container">
+              <div class="flipper">
+                <div class="front">
+                  <!-- front content -->
+                  <div class="card medium">
+                    <div class="card-content">
+                      <h3 class="center-align">
+                        Divers
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="back">
+                  <!-- back content -->
+                  <div class="card medium z-depth-4">
+                    <div class="card-content">
+                      <span class="card-title">Divers</span>
+                      <ul>
+                        <li>
+                          <p>
+
+                          </p>
+
+                        </li>
+                        <li>
+                          <p>
+
+                          </p>
+
+                        </li>
+                        <li>
+                          <p>
+
+                          </p>
+
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
+
       <div class="divider"></div>
-      <section id="work">
-        Réalisation
+
+      <section id="work" class="scrollspy">
+        Réalisations
       </section>
+
       <div class="divider"></div>
-      <section id="resume">
+
+      <section id="resume" class="scrollspy">
         CV express
       </section>
+
       <div class="divider"></div>
-      <section id="contact">
+
+      <section id="contact" class="scrollspy">
         Contact
       </section>
 
