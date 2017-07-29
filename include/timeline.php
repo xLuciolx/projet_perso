65d4c3db33ef5cce2296063d2401223088b8cf28
Loïc Gallay <lgallay@orange.fr>
2017-07-29 21:18:34 +0200

    Add timeline.php

---
+++ include/timeline.php
@@ -0,0 +1,34 @@
+<div class="col s12 m6 l6">
+  <ul class="timeline">
+    <li>
+      <div class="direction-l">
+        <div class="flag-wrapper">
+          <span class="hexa"></span>
+          <span class="flag">Superviseur d'étude</span>
+          <span class="time-wrapper"><span class="time">2007 - 2013</span></span>
+        </div>
+        <div class="desc">Superviseur d'étude chez MDS Parma service: suivi des études, préparation des plans d'études, des rapports préliminaires.</div>
+      </div>
+    </li>
+    <li>
+      <div class="direction-l">
+        <div class="flag-wrapper">
+          <span class="hexa"></span>
+          <span class="flag">Rédacteur technique</span>
+          <span class="time-wrapper"><span class="time">2006 - 2007</span></span>
+        </div>
+        <div class="desc">Rédacteur technique chez MDS Pharma service: vérification des données brutes, édition des résultats, rédaction des rapports</div>
+      </div>
+    </li>
+    <li>
+      <div class="direction-l">
+        <div class="flag-wrapper">
+          <span class="hexa"></span>
+          <span class="flag">Standardiste</span>
+          <span class="time-wrapper"><span class="time">2000 - 2006</span></span>
+        </div>
+        <div class="desc">Standardiste à la Croix Rouge Française: saisie des demandes d'admission, classement des résultats d'analyses médicales.</div>
+      </div>
+    </li>
+  </ul>
+</div>
