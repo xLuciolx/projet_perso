5016eb5f5e512ce38ef1cefd566d532a1115a4a1
Loïc Gallay <lgallay@orange.fr>
2017-08-01 16:42:03 +0200

    Add mentions.php

---
+++ include/mentions.php
@@ -0,0 +1,46 @@
+<div class="col s12 m12 l12 left-align">
+
+  <h4 class="center-align">
+    Mentions légales
+  </h4>
+
+  <p>
+    Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site les informations suivantes :
+  </p>
+
+  <h5>
+    Informations légales
+  </h5>
+
+  <p>
+    Statut du propriétaire : particulier. <br>
+    Propriétaire, créateur, responsable de publication, webmaster : Loïc Gallay - 69380 Lozanne - lgallay@orange.fr <br>
+    Le responsable de la publication est une personne physique.<br>
+    Hébergeur : adresse hébergeur
+  </p>
+
+  <h5>
+    Accessibilité
+  </h5>
+  <p>
+    Le site est par principe accessible aux utilisateurs 24/24h, 7/7j, sauf interruption, programmée ou non, pour les besoins de sa maintenance ou en cas de force majeure. En cas d’impossibilité d’accès au service, le webmaster s’engage à faire son maximum afin de rétablir l’accès au service.
+  </p>
+
+  <h5>
+    Propriété intellectuelle
+  </h5>
+
+  <p>
+    Loïc Gallay est propriétaire exclusif de tous les droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, tant sur la structure que sur les textes, images, graphismes, logo, icônes, sons, logiciels… <br>
+    Toute reproduction totale ou partielle du site www.monsite-perso, représentation, modification, publication, adaptation totale ou partielle de l'un quelconque de ces éléments, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Loïc Gallay, propriétaire du site à l'email : lgallay@orange.fr, à défaut elle sera considérée comme constitutive d’une contrefaçon et passible de poursuite conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+  </p>
+
+  <h5>
+    Liens hypertextes et cookies
+  </h5>
+  <p>
+    Le site www.monsite-perso contient un certain nombre de liens hypertextes vers d’autres sites (partenaires, informations) mis en place avec l’autorisation de Loïc Gallay. Cependant, Loïc Gallay n’a pas la possibilité de vérifier l'ensemble du contenu des sites ainsi visités et décline donc toute responsabilité de ce fait quand aux risques éventuels de contenus illicites.<br>
+    L’utilisateur est informé que lors de ses visites sur le site, un ou des cookies sont susceptibles de s’installer automatiquement sur son ordinateur par l'intermédiaire de son navigateur. Un cookie est un bloc de données qui ne permet pas d'identifier l'utilisateur, mais qui enregistre des informations relatives à la navigation de celui-ci sur le site.<br>
+    Le paramétrage du navigateur permet d’informer de la présence de cookie et éventuellement, de la refuser de la manière décrite à l’adresse suivante : www.cnil.fr. L’utilisateur peut toutefois configurer le navigateur de son ordinateur pour refuser l’installation des cookies, sachant que le refus d'installation d'un cookie peut entraîner l’impossibilité d’accéder à certains services.
+  </p>
+</div>
