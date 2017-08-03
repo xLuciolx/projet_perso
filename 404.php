77cca26d8f48007b8a62b38900e2c91487511329
Loïc Gallay <lgallay@orange.fr>
2017-08-03 16:09:41 +0200

    Add 404.php

---
+++ 404.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="CV développeur intégrateur web: html, css, javascript, php, sql.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>404</title>
+  <!--Import Google Icon Font-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <!-- normalize -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <!-- Materialize -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+  <!-- css 404 -->
+  <link rel="stylesheet" href="css/404.css">
+</head>
+<body>
+
+  <nav class="center-align blue-grey lighten-1">
+    <div class="container">
+      <a href="index.php">Retour</a>
+    </div>
+  </nav>
+  <div class="col s12 m12 l12">
+    <div class="four-o-four">
+      Oups !
+    </div>
+  </div>
+</body>
+</html>
