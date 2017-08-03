5c7394b694129a3119bea484ed1975a07e558e39
Loïc Gallay <lgallay@orange.fr>
2017-08-03 16:07:52 +0200

    Add createCard function

--- include/tool.php
+++ include/tool.php
@@ -20,6 +20,31 @@
         echo '<i class="material-icons ' . $option . '">' . $icon . '</i>';
       }
     }
+  }
 
+
+  /*********************************************************
+   * Creation d'une carte à partir d'un tableau de données *
+   * @param  array  $data [tableau]                        *
+   * @return [html code]       [carte materialize]         *
+   *********************************************************/
+
+
+  function createCard(array $data){
+    ?>
+    <div class="card hoverable">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="<?= $data['src']; ?>" alt="<?= $data['alt']; ?>">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4"><?php echo $data['title']; echo setIcon('more_vert', 1, 'right'); ?></span>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4"><?php echo $data['title']; echo setIcon('close', 1, 'right'); ?></span>
+        <p><?= $data['description']; ?></p>
+        <p><a href="<?= $data['href']; ?>" target="_blank">Lien</a></p>
+      </div>
+    </div>
+  <?php
   }
  ?>
